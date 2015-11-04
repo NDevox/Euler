9f8bd80a4c84a6e9502729a08794e63a43d2b7d4
Nick <nick.a.sarbicki@gmail.com>
2015-11-04 10:47:32 +0000

    made euler_55 a bit clearer

--- euler_55.py
+++ euler_55.py
@@ -1,12 +1,12 @@
 total = 0
 
-for num in range(10001):
-    current_num = num + int(str(num)[::-1])  # So I find this weird, we don't count the number itself,
-    for i in range(50):
-        if str(current_num) == str(current_num)[::-1]:
-            break
-        elif i == 49:
-            total += 1;
-        else:
-            current_num += int(str(current_num)[::-1])
+for num in range(1,10001):  # loop to 10k (python doesn't include the last number)
+    current_num = num + int(str(num)[::-1])  # skip the first iteration.
+    for i in range(50):  # iterate 50 times!
+        if str(current_num) == str(current_num)[::-1]:  # is it a palindrome?
+            break  # yes, break the loop
+        else:  
+            current_num += int(str(current_num)[::-1])  # otherwise, add the reversed number.
+    else:  # we reached the final iteration and no luck
+        total += 1  # so add to the total of lychrels.
 print(total)
