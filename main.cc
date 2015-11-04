38889b5df7dfd4a14dfb45ea3fe76e59d5f789fe
ndevox <nick.a.sarbicki@gmail.com>
2015-11-04 22:58:44 +0000

    adding 55 for cpp

--- main.cc
+++ main.cc
@@ -10,17 +10,19 @@
 #include "9.h"
 #include "10.h"
 #include "11.h"
+#include "55.h"
 
 int main(){
 	printf("The result of Euler 1 is: %i\n", euler_1());
 	printf("The result of Euler 2 is: %i\n", euler_2());
-	printf("The result of Euler 3 is: %i\n", euler_3());
+	printf("The result of Euler 3 is: %f\n", euler_3());
 	printf("The result of Euler 4 is: %i\n", euler_4());
 	printf("The result of Euler 5 is: %i\n", euler_5());
 	printf("The result of Euler 6 is: %i\n", euler_6());
 	printf("The result of Euler 7 is: %i\n", euler_7());
-	printf("The result of Euler 8 is: %ld\n", euler_8()); // requires to print long as number is too big for an int.
+	printf("The result of Euler 8 is: %lld\n", euler_8()); // requires to print long as number is too big for an int.
 	printf("The result of Euler 9 is: %i\n", euler_9());
 	printf("The result of Euler 10 is: %ld\n", euler_10());
 	printf("The result of Euler 11 is: %i\n", euler_11());
+	printf("The result of Euler 11 is: %i\n", euler_55());
 }
